215ee0b078c7043a24a82a5127762a0dc16cfa6e
Djejlan <xhejxhej@gmail.com>
2022-05-07 02:14:04 +0200

    first comit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+//GLOBALS      = No wondow
+//__dirname    - path to v+current directory
+//__filename   - file name
+//require      - fonction to use modules(CommonJS)
+//module       - info about current module(file)
+//process      - info about env where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
